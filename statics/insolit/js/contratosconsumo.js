f6beba1acdf9d7a8966f98dd28f55c310bb79164
Masaru Ohashi Júnior <masaru@vindixit.com>
2020-11-03 11:30:24 -0300

    bugfix edição de contratos de consumo

--- statics/insolit/js/contratosconsumo.js
+++ statics/insolit/js/contratosconsumo.js
@@ -4,21 +4,21 @@ function editContratoConsumo(e) {
     var contratoId = e.parentNode.parentNode.childNodes[3].innerText;
     var contratoConcessionariaId = e.parentNode.parentNode.childNodes[3].childNodes[1].value;
     // var concessionariaName = e.parentNode.parentNode.childNodes[5].innerText;
-    var concessionariaId = e.parentNode.parentNode.childNodes[5].childNodes[1].value;
+    var clienteId = e.parentNode.parentNode.childNodes[5].childNodes[1].value;
+    var concessionariaId = e.parentNode.parentNode.childNodes[7].childNodes[1].value;
     // var clienteName = e.parentNode.parentNode.childNodes[7].innerText;
-    var clienteId = e.parentNode.parentNode.childNodes[7].childNodes[1].value;
     var unidadeConsumidora = e.parentNode.parentNode.childNodes[9].innerText;
-    var vencimentoEm = e.parentNode.parentNode.childNodes[11].innerText;
-    var assinaturaEm = e.parentNode.parentNode.childNodes[13].innerText;
-	var enderecoUnidadeConsumidora = e.parentNode.parentNode.childNodes[13].childNodes[1].value;
-	document.getElementById('ContratoIdForUpdate').value = contratoId;
+	var enderecoUnidadeConsumidora = e.parentNode.parentNode.childNodes[9].childNodes[1].value;
+    var assinaturaEm = e.parentNode.parentNode.childNodes[11].innerText;
+    var vencimentoEm = e.parentNode.parentNode.childNodes[13].innerText;
+	document.getElementById('ContratoConsumoIdForUpdate').value = contratoId;
     document.getElementById('ClienteIdForUpdate').value = clienteId;
     document.getElementById('ConcessionariaIdForUpdate').value = concessionariaId;
 	document.getElementById('ContratoConcessionariaForUpdate').value = contratoConcessionariaId;
     document.getElementById('UCForUpdate').value = unidadeConsumidora;
     document.getElementById('EnderecoUCForUpdate').value = enderecoUnidadeConsumidora;
+    document.getElementById('AssinaturaEmForUpdate').value = formatarData(assinaturaEm);
 	document.getElementById('VencimentoEmForUpdate').value = vencimentoEm;
-    document.getElementById('AssinaturaEmForUpdate').value = assinaturaEm;
 }
 
 function deleteContratoConsumo(e) {
